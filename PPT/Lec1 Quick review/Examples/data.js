d4f8b72be506518d6693f906742784983aa59ccc
TarekSherif <ENG.TAREK.SHERIF@gmail.com>
2019-05-25 14:06:27 +0200

    EX

---
+++ PPT/Lec1 Quick review/Examples/data.js
@@ -0,0 +1,15 @@
+var Emp1 = {
+  Fname: "tarek",
+  LName: "Sherif"
+};
+var 
+var Dept={
+    Title:"it",
+    Manager : {
+        Fname: "Mohamed",
+        LName: "Sherif",
+        address: "sdfsdfsdf",
+        sal: 2323
+      
+      }
+}
